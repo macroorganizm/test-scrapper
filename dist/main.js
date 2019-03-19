40ac4a75644d4b57f6438e6e4e4742d5885a2998
oleg.rybnikov <oleg.rybnikov@valor-software.com>
2019-03-19 11:43:39 +0200

    Updates

--- dist/main.js
+++
@@ -1,2 +0,0 @@
-module.exports = require('./lib/main');
-//# sourceMappingURL=main.js.map
--- lib/settings.ts
+++
@@ -1,9 +0,0 @@
-export const settings = {
-  excludedDomains: [
-    'www.googletagmanager.com',
-    'googletagmanager.com',
-    'google-analytics.com',
-    'ajax.googleapis.com',
-    'fonts.googleapis.com'
-  ]
-};
